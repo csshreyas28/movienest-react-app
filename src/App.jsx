120f560c4e98e345ab5f831e2cfc794c1c6625a6
Shreyas C.S. <csshreyas28@gmail.com>
2025-04-20 21:42:18 +0530

    conditional rendering - 1st method

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import MovieCard from './components/MovieCard'
 
 function App() {
 
+  const movieNumber = 1;
+
   return (
     <> 
-    <MovieCard movie={{title: "Interstellar", release_date: "2013"}}/>
-    <MovieCard movie={{title: "Kal Ho Na Ho", release_date: "2009"}}/>
-
-   </>
+    {movieNumber === 1 ? (
+      <MovieCard movie={{title: "Interstellar", release_date: "2013"}}/>
+     ) : (<MovieCard movie={{title: "Kal Ho Na Ho", release_date: "2009"}}/>
+    )}
+    </>
   )
 }
 
