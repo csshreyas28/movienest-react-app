fe5d06d6a79a19923724596e02af9678fa282fd2
Shreyas C.S. <csshreyas28@gmail.com>
2025-04-20 21:46:05 +0530

    conditional rendering - 2nd method - using short circuit (&&) operator

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,21 @@ import MovieCard from './components/MovieCard'
 
 function App() {
 
-  const movieNumber = 1;
+  const movieNumber = 2;
 
   return (
     <> 
-    {movieNumber === 1 ? (
+    {/* {movieNumber === 1 ? (
       <MovieCard movie={{title: "Interstellar", release_date: "2013"}}/>
      ) : (<MovieCard movie={{title: "Kal Ho Na Ho", release_date: "2009"}}/>
+    )} */}
+
+    {movieNumber === 1 && (
+      <MovieCard movie={{title: "Interstellar", release_date: "2013"}}/>
+    )}
+
+    {movieNumber === 2 && (
+      <MovieCard movie={{title: "Kal Ho Na Ho", release_date: "2009"}}/>
     )}
     </>
   )
