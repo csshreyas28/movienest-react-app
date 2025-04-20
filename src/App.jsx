e657925d9990ce5dd46a4adc88babc1caf6f4b38
Shreyas C.S. <csshreyas28@gmail.com>
2025-04-20 23:15:21 +0530

    NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import './App.css'
 import Favorites from './pages/Favorites';
 import Home from './pages/Home';
 import {Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
 
 function App() {
 
   return (
+    <div>
+      <NavBar/>
     <main className='main-content'>
       <Routes>
         <Route path='/' element={<Home/>}></Route>
         <Route path='/favorites' element={<Favorites/>}></Route>
       </Routes>
     </main>
+    </div>
   )
 }
 
